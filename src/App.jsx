72c0a95b7d0ba44544b946901a92b2760555a103
RenneJ <renne.jamsen@gmail.com>
2023-09-23 16:53:29 +0300

    'Todolist sovellus' tehty

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [item, setItem] = useState("");
+  const [item, setItem] = useState({description: '', date: ''})
   const [itemList, setItemList] = useState([]);
+
+  const changeState = (event) => {
+    setItem({...item, [event.target.name]: event.target.value});
+  }
+
+  function addTask() {
+    event.preventDefault();
+    setItemList([...itemList, item]);
+    setItem({description:'', date:''});
+  }
+
+  const printTasks = itemList.map((item) => 
+        <tr>
+          <td>{item.date}</td>
+          <td>{item.description}</td>
+        </tr>
+
+  );
 
   return (
     <div>
-      This is test commit!
+      <form onSubmit={addTask}>
+      <fieldset>
+        <legend>Add todo</legend>
+        <label>Description: </label>
+        <input type="text" name="description" value={item.description} onChange={changeState}/>
+        <label>Date: </label>
+        <input type="text" name="date" value={item.date} onChange={changeState}/>
+        <input id="add-btn" type="submit" value="Add" />
+      </fieldset>
+      </form>
+      <table>
+        <tbody>
+          <tr>
+            <td><h3>Date</h3></td>
+            <td><h3>Description</h3></td>
+          </tr>
+          {printTasks}
+        </tbody>
+      </table>
     </div>
   )
 }
